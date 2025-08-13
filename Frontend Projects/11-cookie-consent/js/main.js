d0380e3b45273e8c5f897e744e43e132ed1715ab
Theju <66717040+theju-bot@users.noreply.github.com>
2025-08-13 20:25:22 +0530

    Update main.js

--- Frontend Projects/11-cookie-consent/js/main.js
+++ Frontend Projects/11-cookie-consent/js/main.js
@@ -2,5 +2,30 @@ const x = document.querySelector(".x");
 const button = document.querySelector("button");
 const cookie = document.querySelector(".cookie");
 
-button.addEventListener("click",(event) => cookie.classList.add("hide"));
-x.addEventListener("click",(event) => cookie.classList.add("hide"));
+if (localStorage.getItem("cookie") !== "true") {
+    button.addEventListener("click", (event) => {
+        cookie.classList.add("hide");
+        localStorage.setItem("cookie", "true");
+    });
+    x.addEventListener("click", (event) => {
+        cookie.classList.add("hide");
+        localStorage.setItem("cookie", "true");
+    });
+} else {
+    cookie.classList.add("hide");
+}
+
+// Cookie consent stops hiding on reopening the page
+
+/* if (sessionStorage.getItem("cookie") !== "true") {
+    button.addEventListener("click", (event) => {
+        cookie.classList.add("hide");
+        sessionStorage.setItem("cookie", "true");
+    });
+    x.addEventListener("click", (event) => {
+        cookie.classList.add("hide");
+        sessionStorage.setItem("cookie", "true");
+    });
+} else {
+    cookie.classList.add("hide");
+} */
